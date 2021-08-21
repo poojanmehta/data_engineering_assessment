e514de74b7a2b300932db02c9073599a8f5e7c7d
poojanmehta <poojanmehta2000@gmail.com>
2021-08-22 00:54:07 +0530

    creating tables for all the unique countries

--- deassessment.py
+++ deassessment.py
@@ -27,3 +27,17 @@ print(customer_data)
 all_countries = customer_data[3].unique()
 
 print(all_countries)
+
+# thil loop is to create table for all countries
+# iterate n time where n=total unique countries
+for country in all_countries:
+    
+    # prepare query for creating table 
+    # this is dynamic query and change the table name according to the country
+    create_table = "CREATE TABLE IF NOT EXISTS `{table}` (`id` int NOT NULL PRIMARY KEY, `cust_name` VARCHAR(50), `city` VARCHAR(50))".format(table = country)
+    
+    # execute query
+    cursor.execute(create_table)
+    
+    # printing all the successful table creation
+    print(country, " table created successfully")
